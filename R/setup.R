aa21c8a7365f4ff4e95975de652320c6444d2b3d
Leon Steinmeier <Lenostatos@gmx.de>
2020-05-29 16:44:24 +0200

    In Leon's folder: Setup package management with renv and install packages.
    The R code used for those steps is documented in R/setup.R.

---
+++ R/setup.R
@@ -0,0 +1,9 @@
+install.packages("renv")
+renv::init()
+
+install.packages("lidR")
+install.packages("gstat")
+
+install.packages("../../RPackages/crownsegmentr_0.0.0.9000_R_x86_64-pc-linux-gnu.tar.gz")
+
+install.packages("tidyverse")
