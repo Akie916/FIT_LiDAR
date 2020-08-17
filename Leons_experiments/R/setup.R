b978fc3b111d86648cd59a240130cdb9948653ac
Leon Steinmeier <Lenostatos@gmx.de>
2020-08-17 17:52:41 +0200

    In Leon's folder: Update lidR.

--- Leons_experiments/R/setup.R
+++ Leons_experiments/R/setup.R
@@ -4,6 +4,8 @@ renv::init()
 install.packages("lidR")
 install.packages("gstat")
 
-install.packages("../../RPackages/crownsegmentr_0.0.0.9000_R_x86_64-pc-linux-gnu.tar.gz")
+install.packages(
+  "../../../RPackages/crownsegmentr_0.0.0.9000_R_x86_64-pc-linux-gnu.tar.gz"
+)
 
 install.packages("tidyverse")
